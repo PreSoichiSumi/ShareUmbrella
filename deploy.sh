cd60b1693a0892215d99c5c91b5997e8aed2e9a3
SoichiSumi <s-sumi@ist.osaka-u.ac.jp>
2016-01-14 12:27:25 +0000

    modify .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,8 @@ sudo apt-get install sshpass -qq
 eval `ssh-agent -s`
 openssl aes-256-cbc -K $encrypted_6aecf171dcc3_key -iv $encrypted_6aecf171dcc3_iv -in data.pem.enc -out data.pem -d
 ls -al
-sudo ssh-keygen -R ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com
 sudo chmod 400 data.pem
-sudo -E scp -Bv -i data.pem shareUmbrella.war ec2-user@ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com:/home/ec2-user
+sudo -E scp -o StristHostkeyChecking=no -o UserKnownHostFile=/dev/null -Bv -i data.pem shareUmbrella.war ec2-user@ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com:/home/ec2-user
 sudo -E sshpass -p shareumbrella ssh -i data.pem root@ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com
 cd /home/ec2-user
 sudo rm -rd /var/lib/tomcat7/shareUmbrella
