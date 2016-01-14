1e948af935327ac4bc2b89c69b950486063e2ddc
Jiachen Yang <jc-yang@ist.osaka-u.ac.jp>
2016-01-14 22:36:48 +0900

    fixing sshpass

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,9 @@ echo ssh
 ssh -o StrictHostKeyChecking=no -i data.pem ec2-user@ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com echo ok
 echo scp
 scp -Bv -i data.pem shareUmbrella.war ec2-user@ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com:/home/ec2-user
-echo ssh
-sshpass -p shareumbrella ssh -i data.pem ec2-user@ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com
-cd /home/ec2-user
-sudo rm -rd /var/lib/tomcat7/shareUmbrella
-sudo mv shareUmbrella.war /var/lib/tomcat7/webapps/
-sudo service tomcat7 restart
-
+echo rm -rd
+sshpass -p shareumbrella ssh -i data.pem ec2-user@ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com sudo rm -rd /var/lib/tomcat7/shareUmbrella
+echo mv
+sshpass -p shareumbrella ssh -i data.pem ec2-user@ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com sudo mv /home/ec2-user/shareUmbrella.war /var/lib/tomcat7/webapps/
+echo restart
+sshpass -p shareumbrella ssh -i data.pem ec2-user@ec2-54-238-225-208.ap-northeast-1.compute.amazonaws.com sudo service tomcat7 restart
